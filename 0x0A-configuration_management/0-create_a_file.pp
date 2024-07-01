e27ca46fa3a61e63769b56fce5c2ce783e33f6ef
jones0chikwezga <joneschikwezga4@gmail.com>
2024-07-01 03:17:53 -0700

    0x0A-configuration_management#

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file in /tmp using puppet
+file {'crypto_tech_coder':
+  path    =>'/tmp/school',
+  mode    =>'0744',
+  owner   =>'www-data',
+  group   =>'www-data',
+  content =>'I love Puppet',
+}
