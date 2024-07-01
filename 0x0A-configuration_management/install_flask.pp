02d701b63fcfc1f9c92482457c521fbacf5f250f
jones0chikwezga <joneschikwezga4@gmail.com>
2024-07-01 04:08:51 -0700

    x0A-configuration_management#

--- 0x0A-configuration_management/install_flask.pp
+++ 0x0A-configuration_management/install_flask.pp
@@ -13,7 +13,7 @@ package { 'python3-pip':
 
 # Ensure Flask 2.1.0 is installed using pip3
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
+  command => '/usr/local/bin/pip3 install flask==2.1.0',
   unless  => '/usr/local/bin/pip3 show flask | grep Version | grep -q 2.1.0',
   require => Package['python3-pip'],
 }
